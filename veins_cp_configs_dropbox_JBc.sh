416273264c0593ccf449833b6fcb858e1a3576f6
João Batista <joao42lbatista@gmail.com>
2016-10-20 11:47:21 -0200

    Adding header

--- veins_cp_configs_dropbox_JBc.sh
+++ veins_cp_configs_dropbox_JBc.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 #
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para maiores detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Cria uma pasta no Dropbox ($dropboxFolderDest) com as
+# configurações necessárias (*.ini, *.ned etc) e depois zip essa pasta (veins_v001.zip)
+#
+# Last update: 20/10/2016
+#
 initialFolder=$PWD # Get the initial folder
 
 dropboxFolderDest="/media/sda4/prog/Dropbox/z_tmp_cemeai/veins_v001" # Dropbox destination folder
--- veins_cp_full_code_dropbox_JBc.sh
+++ veins_cp_full_code_dropbox_JBc.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 #
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para maiores detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Cria um versão zipada do pasta veins_simulation em um pasta
+# no Dropbox (dropboxFolderD)
+#
+# Last update: 20/10/2016
+#
 initialFolder=$PWD # Get the initial folder
 
 dropboxFolderD="/media/sda4/prog/Dropbox/z_tmp_cemeai/veins_v01" # Dropbox destination folder
