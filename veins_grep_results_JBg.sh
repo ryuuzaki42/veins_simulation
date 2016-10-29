6c7b5cfc024f42221b6c3329e9e58857ff9ab2eb
João Batista <joao42lbatista@gmail.com>
2016-10-29 17:37:42 -0200

    veins_grep_results_JBg.sh grep result by all folder in grep "E$i.*run"

--- veins_grep_results_JBg.sh
+++ veins_grep_results_JBg.sh
@@ -22,112 +22,67 @@
 #
 # Script: Script to collect the simulation result in one place
 #
-# last update: 25/10/2016
+# last update: 29/10/2016
 #
+echo -e "\n## Script to collect the simulation results in one place ##"
+
 if ls -l | grep -q "\.ini"; then
-    echo -e "\nGreat is a OMNet project folder (with a ini file)"
+    echo -e "\nGreat, looks like it ($PWD) is a OMNeT project folder (with a ini file)"
+
+    part=$1
+    if [ "$part" == '' ]; then # result_part
+        echo -e "\nError, you need pass the result_part value, e.g., $0 \"1\" 1 1\n"
+        exit 1
+    fi
+
+    numExpI_1to8=$2
+    if [ "$numExpI_1to8" == '' ]; then # start experiment number
+        echo -e "\nError, you need pass the start experiment number (1 to 8) value, e.g., $0 1 \"1\" 1\n"
+        exit 1
+    fi
+
+    numExpF_1to8=$3
+    if [ "$numExpF_1to8" == '' ]; then # finish experiment number
+        echo -e "\nError, you need pass the finish experiment number (1 to 8) value, e.g., $0 1 1 \"1\"\n"
+        exit 1
+    fi
+
+    if [ "$numExpF_1to8" -lt "$numExpI_1to8" ]; then
+        numExpF_1to8=$numExpI_1to8
+    fi
 
     if echo $PWD | grep -q "vehDist"; then
-        echo "Is a vehDist experiment"
-        experimentName="vehDist"
+        pathFolder="results/vehDist_resultsEnd_$part"
+        echo -e "\n\tIt is a vehDist experiment ($pathFolder)"
     elif echo $PWD | grep -q "epidemic"; then
-        echo "Is a epidemic experiment"
-        experimentName="epidemic"
+        pathFolder="results/epidemic_resultsEnd_$part"
+        echo -e "\n\tIt is a epidemic experiment ($pathFolder)"
     else
-        echo -e "\n$PWD\n\nThis OMNet is unknown"
-        experimentName=''
+        echo -e "\n\tThe ($PWD) is OMNeT project is unknown for this script\nexiting"
+        exit 1
     fi
+    rsu0File="rsu[0]_Count_Messages_Received.r"
+    vehiclesFile="Veh_Messages_Drop.r"
 
-    if [ "$experimentName" != '' ]; then
-        part=$1
-        if [ "$part" == '' ]; then # result_part
-            echo -e "\nError, you need pass the result_part value, e.g., $0 \"1\" 1 1\n"
-            exit 1
-        fi
+    echo -e "\nNumber of experiments: $numExpI_1to8 to $numExpF_1to8"
+    for i in `seq $numExpI_1to8 $numExpF_1to8`; do
+        for experiment in `tree -fdi --noreport $pathFolder | grep "E$i.*run"`; do
+            echo -e "\n## Working in the folder: $experiment"
 
-        numExpI_1to8=$2
-        if [ "$numExpI_1to8" == '' ]; then # start experiment number
-            echo -e "\nError, you need pass the start experiment number (1 to 8) value, e.g., $0 1 \"1\" 1\n"
-            exit 1
-        fi
+            fileRSU=`ls $experiment/$rsu0File`
+            fileVeh=`ls $experiment/$vehiclesFile`
 
-        numExpF_1to8=$3
-        if [ "$numExpF_1to8" == '' ]; then # finish experiment number
-            echo -e "\nError, you need pass the finish experiment number (1 to 8) value, e.g., $0 1 1 \"1\"\n"
-            exit 1
-        fi
+            echo -e "\n\t\t\tFile: $fileRSU"
+            cat $fileRSU | grep -E "Exp:"
 
-        numExpF_DSCR=$4
-        if [ "$numExpF_DSCR" == '' ]; then # total of experiments, for vehDist are 6, for epidemic are 1
-            numExpF_DSCR=6
-        fi
-
-        if [ "$numExpF_1to8" -lt "$numExpI_1to8" ]; then
-            numExpF_1to8=$numExpI_1to8
-        fi
-
-        echo -e "\nNumber of experiments: $numExpI_1to8 to $numExpF_1to8"
-        ((numExpF_1to8++))
-
-        echo -e "\n## Script to collect the simulation results in one place ##"
-
-        if [ "$experimentName" == "vehDist" ]; then
-            pathFolder="results/vehDist_resultsEnd_$part"
-        elif [ "$experimentName" == "epidemic" ]; then
-            pathFolder="results/epidemic_resultsEnd_$part"
-            numExpF_DSCR=1
-            experiment="0099_epidemic"
-        fi
-        echo -e "\n\t## Results from $pathFolder ##\n"
-
-        rsu0File="rsu[0]_Count_Messages_Received.r"
-        vehiclesFile="Veh_Messages_Drop.r"
-
-        experimentNumber=1
-        continueFlag=1
-        while [ $continueFlag == 1 ]; do
-            if [ "$experimentName" == "vehDist" ]; then
-                case $experimentNumber in
-                    1) experiment="0001_chosenByDistance" ;;
-                    2) experiment="0012_chosenByDistance_Speed" ;;
-                    3) experiment="0013_chosenByDistance_Category" ;;
-                    4) experiment="0014_chosenByDistance_RateTimeToSend" ;;
-                    5) experiment="0123_chosenByDistance_Speed_Category" ;;
-                    6) experiment="1234_chosenByDistance_Speed_Category_RateTimeToSend" ;;
-                esac
-            fi
-
-            echo -e "## Values from experiment $experiment ##"
-
-            for folderExpRun in `ls -vA $pathFolder/$experiment/`; do
-
-                for folderRun in `ls -vA $pathFolder/$experiment/$folderExpRun/`; do
-                    echo "Working in the folder: $pathFolder/$experiment/$folderExpRun/"
-
-                    fileRSU=`ls $pathFolder/$experiment/$folderExpRun/$folderRun/$rsu0File`
-                    fileVeh=`ls $pathFolder/$experiment/$folderExpRun/$folderRun/$vehiclesFile`
-
-                    echo -e "\n\t\t## Experiment $experiment $folderExpRun $folderRun ##"
-
-                    echo -e "\t\t\tFile: $fileRSU\n"
-
-                    cat $fileRSU | grep -E "Exp: "$i""
-                    echo
-
-                    echo -e "\t\t\tFile: $fileVeh\n"
-
-                    cat $fileVeh | grep -E "Exp: $i"
-                done
-            done
-
-            echo "experimentNumber $experimentNumber numExpF_DSCR $numExpF_DSCR"
-            if [ $experimentNumber == $numExpF_DSCR ]; then
-                continueFlag=0
-            fi
-            ((experimentNumber++))
+            echo -e "\n\t\t\tFile: $fileVeh"
+            cat $fileVeh | grep -E "Exp:"
         done
-    fi
+    done
+
+    echo -e "\nResult form:"
+    tree -fdi --noreport $pathFolder | grep "E[$numExpI_1to8-$numExpF_1to8].*run"
 else
-    echo -e "\nError: ($PWD) is not a OMNet++ project folder\nPlease go the folder with a ini file"
+    echo -e "\nError: ($PWD) it is not a OMNeT project folder\nPlease go to the folder with a ini file"
 fi
 echo -e "\n\t\t## End of script ##\n"
