8d66c1c05706b5bd8ea004b78177b590a8ca9d90
João Batista <joao42lbatista@gmail.com>
2016-11-16 20:58:12 -0200

    Up readme.r config-template

--- projects/1node/config-template/readme.r
+++ projects/1node/config-template/readme.r
@@ -1,2 +1,8 @@
-Copy the choosen config*.xml to folder above the be used in the projects/
-cp config\ "ChoosenOne" ../config.xml
+## Copy the choosen config*.xml to folder above the be used in the projects/
+    cp config\ "ChoosenOne" ../config.xml
+
+## To use the whit out the obstacles ("noObstacle"), comment the line
+    #*.obstacles.obstacles = xmldoc...
+
+## To use with the obstacles, uncomment the line
+    *.obstacles.obstacles = xmldoc...
