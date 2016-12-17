b490d0d1668830922275720bcb1925a42efcd2c8
João Batista <joao42lbatista@gmail.com>
2016-12-17 11:29:27 -0200

    parse_busRoutes.sh =>Parse values of depart time as the function f(x) = integerPart(x/2), until depart=1260. For example: f(2)=f(3)=1, f(5)=2

---
+++ projects/0scenarios/bologna_small/joined/parse_busRoutes.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para maiores detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Parse values of depart time as the function f(x) = integerPart(x/2),
+# until depart=1260. For example: f(2)=f(3)=1, f(5)=2
+#
+# Last update: 17/12/2016
+#
+fileInput=$1
+if [ "$fileInput" == '' ]; then
+    echo -e "\nError - need pass the file name\n"
+else
+    fileOutput=`echo "$fileInput" | rev | cut -d "." -f2- | rev`_parsed.xml
+
+    echo -e "\nThis script parse value of depart time"
+    echo -e "fileInput: $fileInput\nfileOutput: $fileOutput"
+    echo "The parse is f(x)= integerPart(x/2), e.g., f(2)=f(3)=1 until depart=1260"
+    echo -en "\nWant continue? (y)es - (n)o: "
+    read continueOrNot
+
+    if [ "$continueOrNot" == 'y' ]; then
+        cp "$fileInput" "$fileOutput"
+
+        inValue=0
+                    # (10.5 min + 10.5 min) + 1 s
+        while [ "$inValue" -lt 1261 ]; do
+            outValue=`echo "$inValue/2" | bc`
+
+            echo "Parsing \"$inValue\" to \"$outValue\""
+            sed -i 's/depart=\"'$inValue'\"/depart=\"'$outValue'\"/g' "$fileOutput"
+
+            ((inValue++))
+        done
+
+        echo -e "\nThe result file as saved as: \"$fileOutput\"\n"
+    else
+        echo -e "\nJust exiting\n"
+    fi
+fi
