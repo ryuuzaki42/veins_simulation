c205091a5a3c845d495de4f3fc24c19cb69005c9
João Batista <joao42lbatista@gmail.com>
2017-04-14 23:34:28 -0300

    veins_opp_runall_easy_JBr.sh - corrected some shell script errors

--- JB_scripts/veins_opp_runall_easy_JBr.sh
+++ JB_scripts/veins_opp_runall_easy_JBr.sh
@@ -23,9 +23,9 @@
 # Script: Create the run a easy line to run in the opp_runall
 # Obs: You need veins_opp_runall_JBr.sh
 #
-# last update: 20/11/2016
+# last update: 14/04/2017
 #
-if [ $# -lt 5 ]; then # Test at least tree parameters
+if [ "$#" -lt 5 ]; then # Test at least tree parameters
     echo -e "\nError in the parameters"
     echo "To run: veins_opp_runall_easy_JBr.sh -j3 -f omnet.ini -r 0..2"
     echo " veins_opp_runall_easy_JBr.sh - this script"
@@ -39,17 +39,17 @@ else
     #$4 is -r
     runNumber=$5
 
-    if [ -e $iniFile ]; then
+    if [ -e "$iniFile" ]; then
         export TZ=America/Sao_Paulo # Set timezone to America/Sao_Paulo
 
         fileResultGeneral="results/run_general.r"
         mkdir results 2> /dev/null # create the results folder if not exists
 
-        SimulationStart=`date` # Start simulation
-        SimulationStartSeconds=`date +%s`
-        echo -e "\nSimulation staring at: $SimulationStart\n" | tee $fileResultGeneral
+        SimulationStart=$(date) # Start simulation
+        SimulationStartSeconds=$(date +%s)
+        echo -e "\nSimulation staring at: $SimulationStart\n" | tee "$fileResultGeneral"
 
-        opp_runall $coreNumber veins_opp_runall_JBr.sh -f $iniFile -r $runNumber
+        opp_runall "$coreNumber" veins_opp_runall_JBr.sh -f "$iniFile" -r "$runNumber"
         # Example: opp_runall -j3 veins_opp_runall_JBr.sh -f omnet.ini -r 0..2
         # opp_runall command from OMNeT
         # -j3 j(number cores) to be used
@@ -57,15 +57,15 @@ else
         # -f omnet.ini file
         # -r 0..2 run to execute
 
-        SimulationEnd=`date` # End simulation
-        echo -e "Simulation ends at: $SimulationEnd" | tee -a $fileResultGeneral
+        SimulationEnd=$(date) # End simulation
+        echo -e "Simulation ends at: $SimulationEnd" | tee -a "$fileResultGeneral"
 
-        SimulationEndSeconds=`date +%s`
-        SimulationDiffSeconds=`echo "($SimulationEndSeconds - $SimulationStartSeconds)" | bc`
-        SimulationDiffMin=`echo "scale=2; $SimulationDiffSeconds/60" | bc`
-        SimulationDiffHour=`echo "scale=2; $SimulationDiffSeconds/(60*60)" | bc`
-        echo -e "\nTime spent in simulation: $SimulationDiffSeconds s - or $SimulationDiffMin min - or $SimulationDiffHour h\n" | tee -a $fileResultGeneral
+        SimulationEndSeconds=$(date +%s)
+        SimulationDiffSeconds=$(echo "($SimulationEndSeconds - $SimulationStartSeconds)" | bc)
+        SimulationDiffMin=$(echo "scale=2; $SimulationDiffSeconds/60" | bc)
+        SimulationDiffHour=$(echo "scale=2; $SimulationDiffSeconds/(60*60)" | bc)
+        echo -e "\nTime spent in simulation: $SimulationDiffSeconds s - or $SimulationDiffMin min - or $SimulationDiffHour h\n" | tee -a "$fileResultGeneral"
     else
-        echo -e "\nError: $iniFile not exists\n" | tee $fileResultGeneral
+        echo -e "\nError: $iniFile not exists\n" | tee "$fileResultGeneral"
     fi
 fi
