a065bfb91b0dc17032e448c342bafb289c3ec9c6
João Batista <joao42lbatista@gmail.com>
2017-04-14 22:58:28 -0300

    JB_infoInstall_loadBash.r - corrected some shell script errors

--- JB_scripts/JB_infoInstall_loadBash.r
+++ JB_scripts/JB_infoInstall_loadBash.r
@@ -20,11 +20,10 @@
 #
 # Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #
-# Script: Script that load config Veins/OMNeT and SUMO needed
+# Script: Script that load configuration Veins/OMNeT and SUMO needed
 #
-# Last update: 01/02/2017
+# Last update: 14/04/2017
 #
-
 ## Veins add in ~/.bashrc - Path can be different in your PC
 #. /media/sda2/prog/git_clone/8_veins_simulation/JB_scripts/JB_infoInstall_loadBash.r # copy and uncomment (don't remove the . (dot))
 
@@ -34,47 +33,47 @@
 #   Veins = 4.4
 
 ## Veins run by ~/.bashrc - Can be different in your PC
-TCL_LIBRARY="/usr/lib64/tcl8.6"
-SUMO_HOME="/opt/sumo-0.25.0"
-omnetFolder="/opt/omnetpp-4.6/bin"
-veinsFolder="/media/sda2/prog/git_clone/8_veins_simulation"
+TCL_LIBRARY="/usr/lib64/tcl8.6/"
+SUMO_HOME="/opt/sumo-0.25.0/"
+omnetFolder="/opt/omnetpp-4.6/bin/"
+veinsFolder="/media/sda2/prog/git_clone/8_veins_simulation/"
 
 ## No need of change in your PC
 export PATH=$PATH:$TCL_LIBRARY
 export PATH=$PATH:$SUMO_HOME
-export PATH=$PATH:"$SUMO_HOME/bin"
+export PATH=$PATH:"$SUMO_HOME/bin/"
 export PATH=$PATH:$omnetFolder
 
 export PATH=$PATH:$veinsFolder
-export PATH=$PATH:"$veinsFolder/JB_scripts"
+export PATH=$PATH:"$veinsFolder/JB_scripts/"
 export veinsFolder
 alias veins_folder_cd='cd $veinsFolder'
 
 veins_RunExperiment() {
-    dateStart=`date` # Start runing experiment
+    dateStart=$(date) # Start running experiments
     echo -e "\nExperiments staring at: $dateStart"
 
-    # get inital variables
-    iniFile=$1
-    runExperimentI=$2
-    runExperimentF=$3
+    # get initial variables
+    iniFile="$1"
+    runExperimentI="$2"
+    runExperimentF="$3"
 
     # cd veins folder, make and go back
     veins_folder_cd
-    echo -e "\nMake the files in the folder \"`pwd`/\"\n"
+    echo -e "\nMake the files in the folder \"$(pwd)\"\n"
     make
-    cd -
+    cd - || exit
 
     if [ "$runExperimentI" == '' ]; then # test the input, if is '' will run -r 0, if is valid, run -r I..F
-        runExperimentI=0
-        runExperimentF=0
+        runExperimentI='0'
+        runExperimentF='0'
     fi
 
-    echo -e "\nRunning: opp_run -r $runExperimentI..$runExperimentF -n ../../src/veins/ -u Cmdenv -l ../../out/gcc-debug/src/libveins_simulation.so "$iniFile"\n"
-    opp_run -r $runExperimentI..$runExperimentF -n ../../src/veins/ -u Cmdenv -l ../../out/gcc-debug/src/libveins_simulation.so "$iniFile"
+    echo -e "\nRunning: opp_run -r $runExperimentI..$runExperimentF -n ../../src/veins/ -u Cmdenv -l ../../out/gcc-debug/src/libveins_simulation.so $iniFile\n"
+    opp_run -r "$runExperimentI..$runExperimentF" -n ../../src/veins/ -u Cmdenv -l ../../out/gcc-debug/src/libveins_simulation.so "$iniFile"
 
     echo -e "\n\nExperiment starts date: $dateStart"
-    echo -e "Experiment ends date: `date`\n" # End of running experiment
+    echo -e "Experiment ends date: $(date)\n" # End of running experiment
 }
 alias opp-exc=veins_RunExperiment
 alias opp-exc-res="veins_RunExperiment > results/ExperimentOutput.r"
