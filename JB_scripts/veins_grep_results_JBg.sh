2872dca3243bc220288cfeb3b9bab71227ef73af
João Batista <joao42lbatista@gmail.com>
2018-01-10 15:35:32 -0200

    veins_grep_results_JBg.sh - "\n" to "\\n"; removed cat and use only the grep

--- JB_scripts/veins_grep_results_JBg.sh
+++ JB_scripts/veins_grep_results_JBg.sh
@@ -22,12 +22,12 @@
 #
 # Script: Script to collect the simulation result in one place
 #
-# last update: 15/12/2016
+# last update: 10/01/2018
 #
-echo -e "\n## Script to collect the simulation results in one place ##"
+echo -e "\\n## Script to collect the simulation results in one place ##"
 
-if ls -l | grep -q "\.ini"; then
-    echo -e "\nGreat, looks like it ($PWD) is a OMNeT project folder (with a ini file)"
+if find . | grep -q "\\.ini"; then
+    echo -e "\\nGreat, looks like it ($PWD) is a OMNeT project folder (with a ini file)"
 
     noConfig=$1
     if [ "$noConfig" == "noConfig" ]; then
@@ -36,19 +36,19 @@ if ls -l | grep -q "\.ini"; then
 
     part=$1
     if [ "$part" == '' ]; then # result_part
-        echo -e "\nError, you need pass the result_part value, e.g., $0 \"1\" 1 1\n"
+        echo -e "\\nError, you need pass the result_part value, e.g., $0 \"1\" 1 1\\n"
         exit 1
     fi
 
     numExpI_1to8=$2
     if [ "$numExpI_1to8" == '' ]; then # start experiment number
-        echo -e "\nError, you need pass the start experiment number (1 to 8) value, e.g., $0 1 \"1\" 1\n"
+        echo -e "\\nError, you need pass the start experiment number (1 to 8) value, e.g., $0 1 \"1\" 1\\n"
         exit 1
     fi
 
     numExpF_1to8=$3
     if [ "$numExpF_1to8" == '' ]; then # finish experiment number
-        echo -e "\nError, you need pass the finish experiment number (1 to 8) value, e.g., $0 1 1 \"1\"\n"
+        echo -e "\\nError, you need pass the finish experiment number (1 to 8) value, e.g., $0 1 1 \"1\"\\n"
         exit 1
     fi
 
@@ -56,66 +56,66 @@ if ls -l | grep -q "\.ini"; then
         numExpF_1to8=$numExpI_1to8
     fi
 
-    if echo $PWD | grep -q "mfcv"; then
+    if echo "$PWD" | grep -q "mfcv"; then
         pathFolder="results/mfcv_resultsEnd_$part"
-        echo -e "\n    It is a mfcv experiment ($pathFolder)"
-    elif echo $PWD | grep -q "epidemic"; then
+        echo -e "\\n    It is a mfcv experiment ($pathFolder)"
+    elif echo "$PWD" | grep -q "epidemic"; then
         pathFolder="results/epidemic_resultsEnd_$part"
-        echo -e "\n    It is a epidemic experiment ($pathFolder)"
+        echo -e "\\n    It is a epidemic experiment ($pathFolder)"
     else
-        echo -e "\n    The ($PWD) is OMNeT project is unknown for this script\nexiting"
+        echo -e "\\n    The ($PWD) is OMNeT project is unknown for this script\\nexiting"
         exit 1
     fi
     rsuFile="rsu_Count_Messages_Received.r"
     vehiclesFile="Veh_Messages_Drop.r"
 
-    echo -e "\nNumber of experiments: $numExpI_1to8 to $numExpF_1to8"
-    for i in `seq $numExpI_1to8 $numExpF_1to8`; do
-        for experiment in `tree -fdi --noreport $pathFolder | grep "E$i.*run"`; do
-            echo -e "\n## Working in the folder: $experiment"
+    echo -e "\\nNumber of experiments: $numExpI_1to8 to $numExpF_1to8"
+    for i in $(seq "$numExpI_1to8" "$numExpF_1to8"); do
+        for experiment in $(tree -fdi --noreport "$pathFolder" | grep "E$i.*run"); do
+            echo -e "\\n## Working in the folder: $experiment"
 
-            rsuFileLs=`ls $experiment/$rsuFile`
-            vehiclesFileLs=`ls $experiment/$vehiclesFile`
+            rsuFileLs=$(find "$experiment/$rsuFile")
+            vehiclesFileLs=$(find "$experiment/$vehiclesFile")
 
             if [ "$rsuFileLs" != '' ]; then
-                echo -e "\n            File: $rsuFileLs"
-                cat $rsuFileLs | grep -E "Exp:"
+                echo -e "\\n            File: $rsuFileLs"
+                grep "Exp:" < "$rsuFileLs"
 
                 filesFound+="    $experiment/$rsuFile\\n"
             else
-                echo "\nThe experiment: $experiment\n Don't have the file \"$rsuFileLs\""
+                echo -e "\\nThe experiment: $experiment\\n Don't have the file \"$rsuFileLs\""
                 filesNotFound+="    $experiment/$rsuFile\\n"
             fi
 
             if [ "$vehiclesFileLs" != '' ]; then
-                echo -e "\n            File: $vehiclesFileLs"
-                cat $vehiclesFileLs | grep -E "Exp:"
+                echo -e "\\n            File: $vehiclesFileLs"
+                grep "Exp:" < "$vehiclesFileLs"
 
                 filesFound+="    $experiment/$vehiclesFile\\n"
             else
-                echo "\nThe experiment: $experiment\n Don't have the file \"$vehiclesFileLs\""
+                echo -e "\\nThe experiment: $experiment\\n Don't have the file \"$vehiclesFileLs\""
                 filesNotFound+="    $experiment/$vehiclesFile\\n"
             fi
         done
     done
 
     if [ "$filesFound" != '' ]; then
-        echo -e "\n# Result - Files founded:\n\n$filesFound"
+        echo -e "\\n# Result - Files found:\\n\\n$filesFound"
     else
-        echo -e "\n# None file was founded"
+        echo -e "\\n# No file found"
     fi
 
     if [ "$filesNotFound" != '' ]; then
-        echo -e "\n# Result - Files not founded:\n\n$filesNotFound"
+        echo -e "\\n# Result - Files not found:\\n\\n$filesNotFound"
     else
-        echo -e "\n# All files was founded"
+        echo -e "\\n# All files found"
     fi
 else
-    echo -e "\nError: ($PWD) it is not a OMNeT project folder\nPlease go to the folder with a ini file\n"
+    echo -e "\\nError: ($PWD) it is not a OMNeT project folder\\nPlease go to the folder with a ini file\\n"
     exit 1
 fi
 
 if [ "$noConfig" != "noConfig" ]; then
     veins_save_config_used_JBr.sh inTheTerminal
 fi
-echo -e "\n        ## End of script ##\n"
+echo -e "\\n        ## End of script ##\\n"
