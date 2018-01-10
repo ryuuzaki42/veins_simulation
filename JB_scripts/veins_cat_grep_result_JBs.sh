e68fcf6d1d4b5e73fb8de71b13aef8b3f57c3590
João Batista <joao42lbatista@gmail.com>
2018-01-10 15:36:44 -0200

    veins_cat_grep_result_JBs.sh - "\n" to "\\n"; and organized

--- JB_scripts/veins_cat_grep_result_JBs.sh
+++ JB_scripts/veins_cat_grep_result_JBs.sh
@@ -22,17 +22,17 @@
 #
 # Script: Grep result from a file
 #
-# Last update: 01/02/2017
+# Last update: 10/01/2018
 #
 if [ "$1" != "noHead" ]; then
-    echo -e "\nThis script grep values from a result from a file"
+    echo -e "\\nThis script grep values from a result from a file"
 else
     shift
 fi
 
 fileName=$1
 if [ "$fileName" == '' ]; then
-    echo -e "\nError - need pass the file name to grep\n"
+    echo -e "\\nError - need pass the file name to grep\\n"
 else
     # Rsu
     op1="Total of messages received:"
@@ -55,7 +55,7 @@ else
     op16="Epidemic count RequestMessageVectorSend:"
     op17="Epidemic \(full\) is the sum of those 3:"
 
-    countOption=17
+    countOption="17"
     optionInput=$2
 
     ((countOption++))
@@ -63,52 +63,52 @@ else
         i=1
         while [ "$i" -lt "$countOption" ]; do
             if [ "$i" -eq '1' ]; then
-                echo -e "\n###   Rsu"
+                echo -e "\\n###   Rsu"
             elif [ "$i" -eq '9' ]; then
-                echo -e "\n###   Veh"
+                echo -e "\\n###   Veh"
             fi
 
             opTmp="op$i"
             echo -e "    $i - ${!opTmp}"
             ((i++))
         done
-        echo -e "\n###   General"
+        echo -e "\\n###   General"
         echo "    88 - All above"
         echo "    99 - You insert the value"
 
-        echo -en "\nWich value you want grep: "
-        read optionInput
+        echo -en "\\nWich value you want grep: "
+        read -r optionInput
     fi
 
     if [ "$optionInput" == "88" ]; then
         i=1
         while [ "$i" -lt "$countOption" ]; do
-            $0 noHead $fileName $i
+            "$0" noHead "$fileName" "$i"
             ((i++))
         done
 
         exit 0
     elif [ "$optionInput" == "99" ]; then
-        echo -en "\nInsert the value to be used in the grep: "
-        read grepValue
+        echo -en "\\nInsert the value to be used in the grep: "
+        read -r grepValue
     else
         opTmp="op$optionInput"
-        grepValue=`echo "${!opTmp}"`
+        grepValue=${!opTmp}
     fi
 
     if [ "$grepValue" == '' ]; then
-        echo -e "\n## Error - option \"$optionInput\" is unknown"
+        echo -e "\\n## Error - option \"$optionInput\" is unknown"
     else
-        echo -e "\n## Grep the value: $optionInput - \"${!opTmp}\""
-        grepResult=`cat $fileName | grep -E "$grepValue|## Working in the folder.*run_0" | sed 's/## Working in the folder.*run_0//g'`
+        echo -e "\\n## Grep the value: $optionInput - \"${!opTmp}\""
+        grepResult=$(grep -E "$grepValue|## Working in the folder.*run_0" < "$fileName" | sed 's/## Working in the folder.*run_0//g')
 
         echo "$grepResult"
 
-        echo -e "\n## Values more clean:"
+        echo -e "\\n## Values more clean:"
         echo "$grepResult" | rev | cut -d ':' -f1 | rev | cut -d ' ' -f2-
 
-        if [ $optionInput -eq "1" ]; then
-            echo -e "\n## Values more clean2:"
+        if [ "$optionInput" -eq '1' ]; then
+            echo -e "\\n## Values more clean2:"
             echo "$grepResult" | rev | cut -d ':' -f1 | rev | cut -d ' ' -f6- | sed 's/ % received//g'
         fi
     fi
